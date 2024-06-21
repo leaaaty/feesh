fcc7364e5e1e5a3298c6c701bc45882523ca9ec5
leaaaty <162678699+leaaaty@users.noreply.github.com>
2024-06-21 17:18:25 +0800

    Update jquery1.js

--- jquery1.js
+++ jquery1.js
@@ -24,10 +24,6 @@ var x = setInterval(function() {
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = '<a href="https://www.youtube.com/watch?v=2dH3-QHS-Kc">HBD FINANA</a>';
-        document.getElementById("enterwebsite").innerHTML = '<span onclick="enterWebsite()">finanaryugu.com</a>';
+        document.getElementById("enterwebsite").innerHTML = '<a href="https://finanaryugu.com"> \>> FINANAYRUGU.COM \<< </a>';
     }
 }, 1000);
-
-function enterWebsite() {
-	location.href = "https://finanaryugu.com";
-}
